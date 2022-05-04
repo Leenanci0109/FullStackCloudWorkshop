c37a9106005006bb618b0aee1e03634560150a9b
Leenanci Parmar <58741092+Leenanci0109@users.noreply.github.com>
2022-05-03 20:16:44 -0400

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,10 @@
+str1="Hi"
+str2="Leenanci"
+print ("s1:",str1)
+print ("s2:",str2)
+str3=str1+str2
+print("s1+s2:",str3)
+str4=''.join([str1, str2])
+print(str4)
+str5 = '%s %s' % (str1, str2)
+print(str4)
